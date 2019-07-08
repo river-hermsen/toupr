d6e6b3ad5f4fa296bd0b049827b3a1b8ba0d04dc
River Hermsen <riverhermsen@hotmail.nl>
2019-07-08 17:28:11 +0200

    Working on eslint airbnb config

--- src/config/fireBaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyAE9lkAsdQfYeC5wP81trYQ9JjPI3s3yVs',
-  authDomain: 'toupr-35ecc.firebaseapp.com',
-  databaseURL: 'https://toupr-35ecc.firebaseio.com',
-  projectId: 'toupr-35ecc',
-  storageBucket: '',
-  messagingSenderId: '802190317343',
-  appId: '1:802190317343:web:7bf9e9aaef059982',
-};
