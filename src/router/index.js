9b8d6f92bcb1bda7730d237f838dba08f31b9cf0
River Hermsen <riverhermsen@hotmail.nl>
2019-07-09 19:30:26 +0200

    Created login view and added firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,33 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import firebase from 'firebase';
 import routes from './routes';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in.
+      console.log('User is signed in');
+
+      console.log(user);
+
+      // ...
+    } else {
+      console.log('User is not signed in');
+
+      // User is signed out.
+      // ...
+    }
+    next();
+  });
+});
+
+export default router;
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,21 +1,91 @@
 <template>
-  <div class="container">
+  <div class="container-login">
     <div>
-      <b-field label="Email" type="is-danger" message="This email is invalid">
-        <b-input type="email" value="john@" maxlength="30"></b-input>
+      <b-field
+        label="Email"
+        :class="{ 'is-danger': errors.has('Email') }"
+        :message="errors.first('Email')"
+        :type="{'is-danger': errors.has('Email')}"
+      >
+        <b-input
+          data-vv-name="Email"
+          type="email"
+          placeholder="Email"
+          v-validate="'required|email'"
+          v-model="email"
+        ></b-input>
       </b-field>
     </div>
     <div>
-      <b-field label="Password">
-        <b-input type="password" value="iwantmytreasure" password-reveal></b-input>
+      <b-field
+        label="Password"
+        :class="{ 'is-danger': errors.has('Wachtwoord') }"
+        :message="errors.first('Wachtwoord')"
+        :type="{'is-danger': errors.has('Wachtwoord')}"
+      >
+        <b-input
+          data-vv-name="Wachtwoord"
+          type="password"
+          placeholder="Password"
+          v-validate="'required'"
+          v-model="password"
+        ></b-input>
       </b-field>
+    </div>
+    <div class="password-button-container">
+      <router-link to="/wachtwoordvergeten" class="link-password">Wachtwoord vergeten?</router-link>
+      <b-button type="is-primary" class="login-button-container" @click="handleLogin">Login</b-button>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.container-login {
+  box-shadow: 0 0 40px 3px rgba(0, 0, 0, 0.05);
+  max-width: 600px;
+  margin: 2rem auto;
+  padding: 2rem;
+}
+.password-button-container {
+  margin-top: 1.2rem;
+  height: 30px;
+}
+.link-password {
+  float: left;
+}
+
+.login-button-container {
+  float: right;
+}
 </style>
 
 <script>
-export default {};
+import firebase from 'firebase';
+
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    handleLogin() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+
+          // Handle Errors here.
+          // let errorCode = error.code;
+          // let errorMessage = error.message;
+          // ...
+        });
+    },
+  },
+};
 </script>
